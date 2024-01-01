be6276b8f9aa77af31ef674991cb15176d197b93
Muzaki新联想 <aamuzakii@gmail.com>
2024-01-01 22:15:14 +0700

    alhamdulillah

--- app/api/foo/route.ts
+++ app/api/foo/route.ts
@@ -1,40 +1,28 @@
 import { NextResponse } from "next/server";
-const sharp = require("sharp");
+import sharp from "sharp";
 import axios from "axios";
 
 const example =
   "https://images.ctfassets.net/0wvobgztd3t0/4WCqda0hBPgK8LAyphHxfJ/9aedb4cf264da5d6952f4966fe1fcf16/A_15_-_Photo.jpg";
 
 export async function GET(req: any, res: any) {
-  // const { imageUrl, width, height } = req.body;
-
   try {
-    axios
-      .get(example, { responseType: "arraybuffer" })
-      .then((response) => {
-        const imageData = Buffer.from(response.data, "binary");
+    const response = await axios.get(example, { responseType: "arraybuffer" });
+    const imageData = Buffer.from(response.data, "binary");
 
-        sharp(imageData)
-          .resize(100)
-          .toFormat("webp")
-          .toBuffer()
-          .then((resizedData: any) => {
-            console.log(resizedData.toString("base64"));
-            res.send(resizedData);
+    const resizedData = await sharp(imageData)
+      .resize(100)
+      .toFormat("webp")
+      .toBuffer();
 
-            // Now you have the resized image data as a base64 string
-          })
-          .catch((err: any) => {
-            console.error("Error during image resizing:", err);
-          });
-      })
-      .catch((error) => {
-        console.error("Error during image download:", error);
-      });
+    console.log(resizedData.toString("base64"));
+    return new Response(resizedData, {
+      headers: { "content-type": "image/png" },
+    });
+
+    // Now you have the resized image data as a base64 string
   } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
+    console.error("Error during image processing:", error);
+    return NextResponse.json({ error: "user" });
   }
-
-  return NextResponse.json({ data: "user" });
 }
