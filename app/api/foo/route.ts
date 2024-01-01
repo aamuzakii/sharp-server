8ab5d2affd0e5af47aa24a20adae11eb192e1e69
Muzaki新联想 <aamuzakii@gmail.com>
2024-01-01 22:05:42 +0700

    success POC

--- app/api/foo/route.ts
+++ app/api/foo/route.ts
@@ -1,12 +1,36 @@
 import { NextResponse } from "next/server";
 const sharp = require("sharp");
+import axios from "axios";
+
+const example =
+  "https://images.ctfassets.net/0wvobgztd3t0/4WCqda0hBPgK8LAyphHxfJ/9aedb4cf264da5d6952f4966fe1fcf16/A_15_-_Photo.jpg";
 
 export async function GET(req: any, res: any) {
-  const { imageUrl, width, height } = req.body;
+  // const { imageUrl, width, height } = req.body;
 
   try {
-    const resizedImageBuffer = await sharp(imageUrl)
-      .resize({ width, height })
+    axios
+      .get(example, { responseType: "arraybuffer" })
+      .then((response) => {
+        const imageData = Buffer.from(response.data, "binary");
+
+        sharp(imageData)
+          .resize(100)
+          .toBuffer()
+          .then((resizedData: any) => {
+            console.log(resizedData.toString("base64"));
+            // Now you have the resized image data as a base64 string
+          })
+          .catch((err: any) => {
+            console.error("Error during image resizing:", err);
+          });
+      })
+      .catch((error) => {
+        console.error("Error during image download:", error);
+      });
+
+    const resizedImageBuffer = await sharp(example)
+      .resize({ width: 100, height: 100 })
       .toFormat("webp")
       .toBuffer();
 
