9bff1ccbbdbc7831cd1fbd5339ffd50cd7b99fef
Muzaki新联想 <aamuzakii@gmail.com>
2024-01-03 09:17:06 +0700

    remove GET

--- app/api/foo/route.ts
+++ app/api/foo/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse, NextRequest } from "next/server";
 import sharp from "sharp";
-import axios from "axios";
+import Axios from "axios";
+import { setupCache } from "axios-cache-interceptor";
 import type { NextApiRequest, NextApiResponse } from "next";
 import Cors from "cors";
 
@@ -26,34 +27,6 @@ function runMiddleware(
 const example =
   "https://images.ctfassets.net/0wvobgztd3t0/4WCqda0hBPgK8LAyphHxfJ/9aedb4cf264da5d6952f4966fe1fcf16/A_15_-_Photo.jpg";
 
-export async function GET(req: NextApiRequest, res: any) {
-  if (req.method === "OPTIONS") {
-    // Preflight request. Reply successfully without actually processing the request.
-    res.status(200).end();
-    return;
-  }
-
-  try {
-    const response = await axios.get(example, { responseType: "arraybuffer" });
-    const imageData = Buffer.from(response.data, "binary");
-
-    const resizedData = await sharp(imageData)
-      .resize({ width: 1000, height: 1000, fit: "inside" })
-      .toFormat("webp")
-      .toBuffer();
-
-    console.log(resizedData.toString("base64"));
-    return new Response(resizedData, {
-      headers: { "content-type": "image/png" },
-    });
-
-    // Now you have the resized image data as a base64 string
-  } catch (error) {
-    console.error("Error during image processing:", error);
-    return NextResponse.json({ error: "user" });
-  }
-}
-
 export async function POST(req: NextApiRequest, res: any) {
   await runMiddleware(
     req,
@@ -67,6 +40,9 @@ export async function POST(req: NextApiRequest, res: any) {
 
   let passedValue = await new Response(req.body).text();
   let reqBody = JSON.parse(passedValue);
+
+  const instance = Axios.create();
+  const axios = setupCache(instance);
 
   try {
     const response = await axios.get(reqBody.link, {
