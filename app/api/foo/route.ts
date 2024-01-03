13fecd3c170e00ddfa7c2d5a87a75dcaa2875a70
Muzaki新联想 <aamuzakii@gmail.com>
2024-01-03 07:45:52 +0700

    fit: "inside"

--- app/api/foo/route.ts
+++ app/api/foo/route.ts
@@ -1,11 +1,11 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import sharp from "sharp";
 import axios from "axios";
 
 const example =
   "https://images.ctfassets.net/0wvobgztd3t0/4WCqda0hBPgK8LAyphHxfJ/9aedb4cf264da5d6952f4966fe1fcf16/A_15_-_Photo.jpg";
 
-export async function GET(req: any, res: any) {
+export async function GET(req: NextRequest, res: any) {
   if (req.method === "OPTIONS") {
     // Preflight request. Reply successfully without actually processing the request.
     res.status(200).end();
@@ -17,7 +17,7 @@ export async function GET(req: any, res: any) {
     const imageData = Buffer.from(response.data, "binary");
 
     const resizedData = await sharp(imageData)
-      .resize(100)
+      .resize({ width: 1000, height: 1000, fit: "inside" })
       .toFormat("webp")
       .toBuffer();
 
