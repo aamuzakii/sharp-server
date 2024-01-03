471bb7e733ae69a60fab63f60ae7228cb4a4b2ba
Muzaki新联想 <aamuzakii@gmail.com>
2024-01-03 07:48:22 +0700

    add cors

--- app/api/foo/route.ts
+++ app/api/foo/route.ts
@@ -1,11 +1,32 @@
 import { NextResponse, NextRequest } from "next/server";
 import sharp from "sharp";
 import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
+
+const cors = Cors({
+  methods: ["POST", "GET", "HEAD"],
+});
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 
 const example =
   "https://images.ctfassets.net/0wvobgztd3t0/4WCqda0hBPgK8LAyphHxfJ/9aedb4cf264da5d6952f4966fe1fcf16/A_15_-_Photo.jpg";
 
-export async function GET(req: NextRequest, res: any) {
+export async function GET(req: NextApiRequest, res: any) {
   if (req.method === "OPTIONS") {
     // Preflight request. Reply successfully without actually processing the request.
     res.status(200).end();
@@ -31,4 +52,9 @@ export async function GET(req: NextRequest, res: any) {
     console.error("Error during image processing:", error);
     return NextResponse.json({ error: "user" });
   }
+}
+
+export async function POST(req: NextApiRequest, res: any) {
+  await runMiddleware(req, res, cors);
+  console.log("masuk post");
 }
